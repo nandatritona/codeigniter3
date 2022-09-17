3027e43d9da6de53d6070718d4a16d63bb7a40e8
nandatritona <nandhafebrian3@gmail.com>
2022-09-17 20:18:37 +0700

    selesai setup, tingal ngoding aja kalau ada project nya

--- application/models/M_welcome.php
+++ application/models/M_welcome.php
@@ -1,17 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
-	public function __construct() {
+class M_welcome extends CI_Model {
+    public function __construct() {
 		/**
 		 * untuk error, jika mau di tampilkan matikan comment nya
 		 */
 		// ini_set('display_errors', 'off');
 		parent::__construct();
-	}
-
-	public function index()
-	{
-		$this->load->view('welcome_message');
 	}
 }
